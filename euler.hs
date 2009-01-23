34ebf024dcba52300f8f54798284aad2267888b9
John Evans <lgastako@gmail.com>
2009-01-22 22:05:42 -0800

    more

--- euler.hs
+++ euler.hs
@@ -1,6 +1,6 @@
 import Char (digitToInt)
 import Test.BenchPress (bench)
-import List (sort, nub)
+import List (sort, nub, tails, transpose)
 import System.TimeIt (timeIt)
 
 -- euler #1 
@@ -126,14 +126,15 @@ divisible_by_all :: [Integer] -> Integer -> Bool
 -- ok now it's lazy, dunno if that'll be enough, but we'll see
 divisible_by_all ds n = length (takeWhile (factor n) [d | d <- ds]) == length ds
 
+-- STILL way too slow
+-- took 32 minutes or something, btu eventually got the answer.
+-- 232792560
 euler5 :: Integer
 euler5 = head 
          (filter 
           (divisible_by_all 
            (reverse [11..20]))
           [20..])
-
--- STILL way too slow
 
 
 -- euler #6
@@ -220,6 +221,9 @@ products_of_n_consecutive_digits n s
 -- 40824
 euler8 = maximum (products_of_n_consecutive_digits 5 euler8_input)
 
+-- from the threads (adapted to use my equivalent functions)... very nice.
+euler8_alt = maximum . map (product . take 5) . tails $ string_digits euler8_input
+
 -- euler #9
 --
 -- A Pythagorean triplet is a set of three natural numbers, a b c, for
@@ -286,10 +290,61 @@ even_faster_primes :: [Integer]
 even_faster_primes = 2:3:[next_faster_prime n | n <- [3..]]
 
 euler10 :: Integer
-euler10 = sum (takeWhile (< 2000000) fast_primes)
+euler10 = sum (takeWhile (< 2000000) even_faster_primes)
 
 -- bench 50 $ do putStr (show (length (take 500 primes)))
 -- bench 50 $ do putStr (show (length (take 500 fast_primes)))
+
+-- euler #11
+--
+-- In the 2020 grid below, four numbers along a diagonal line have
+-- been marked in red.
+--
+-- [grid redacted]
+--
+-- The product of these numbers is 26 * 63 * 78 * 14 = 1788696.
+--
+-- What is the greatest product of four adjacent numbers in any
+-- direction (up, down, left, right, or diagonally) in the 20x20 grid?
+
+--euler11 = map (take 4) (tails euler11_rows)
+
+five_at_a_time xs = filter ((>= 5) . length) (map (take 5) (tails xs))
+products_of_five_at_a_time xs = map product (five_at_a_time xs)
+max_products_of_five_at_a_time = maximum . products_of_five_at_a_time
+e11_row_maxes = map max_products_of_five_at_a_time euler11_rows
+e11_col_maxes = map max_products_of_five_at_a_time euler11_cols
+e11_max_row_product = maximum e11_row_maxes
+e11_max_col_product = maximum e11_col_maxes
+e11_max_diag_product = 0 -- TODO: This
+
+euler11 = maximum [e11_max_col_product, e11_max_row_product, e11_max_diag_product]
+
+
+-- sort of cheating, but whatever...
+euler11_rows = [
+ [08, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 77, 91, 08],
+ [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 04, 56, 62, 00],
+ [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 03, 49, 13, 36, 65],
+ [52, 70, 95, 23, 04, 60, 11, 42, 69, 24, 68, 56, 01, 32, 56, 71, 37, 02, 36, 91],
+ [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
+ [24, 47, 32, 60, 99, 03, 45, 02, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
+ [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
+ [67, 26, 20, 68, 02, 62, 12, 20, 95, 63, 94, 39, 63, 08, 40, 91, 66, 49, 94, 21],
+ [24, 55, 58, 05, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+ [21, 36, 23, 09, 75, 00, 76, 44, 20, 45, 35, 14, 00, 61, 33, 97, 34, 31, 33, 95],
+ [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 03, 80, 04, 62, 16, 14, 09, 53, 56, 92],
+ [16, 39, 05, 42, 96, 35, 31, 47, 55, 58, 88, 24, 00, 17, 54, 24, 36, 29, 85, 57],
+ [86, 56, 00, 48, 35, 71, 89, 07, 05, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
+ [19, 80, 81, 68, 05, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 04, 89, 55, 40],
+ [04, 52, 08, 83, 97, 35, 99, 16, 07, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
+ [88, 36, 68, 87, 57, 62, 20, 72, 03, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
+ [04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 08, 46, 29, 32, 40, 62, 76, 36],
+ [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 04, 36, 16],
+ [20, 73, 35, 29, 78, 31, 90, 01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 05, 54],
+ [01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 01, 89, 19, 67, 48]]
+
+euler11_cols = transpose euler11_rows
 
 -- euler #12
 --
@@ -634,3 +689,26 @@ e30_nums = [x | x <- natural_numbers, qualifies_for_e30 x 5]
 -- 443839
 euler30 = sum(take 6 e30_nums)
 
+
+-- euler 34
+--
+-- 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+-- 
+-- Find the sum of all numbers which are equal to the sum of the
+-- factorial of their digits.
+-- 
+-- Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+--
+
+
+
+-- euler #48
+--
+-- The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+-- 
+-- Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+
+self_pow n = n ^ n
+e48_series = map self_pow [1..]
+e48_sum = sum (take 1000 e48_series)
+euler48 = reverse (take 10 (reverse (show e48_sum)))
